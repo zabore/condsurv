92761e9c9d64423ebfa678866f39bfb88e5333a4
Emily Zabor <emily.zabor@gmail.com>
2019-03-07 11:38:40 -0500

    minor edits to condKMest and condKMggplot

--- R/condKMggplot.R
+++ R/condKMggplot.R
@@ -3,56 +3,72 @@
 #' \code{condKMggplot} produces a Kaplan-Meier plot for a variety of times on
 #' which to condition using ggplot2
 #'
-#' @param .basekm \code{survfit} object
-#' @param .at vector of times on which to condition
-#' @param .main plot title
-#' @param .xlab x-axis label
-#' @param .ylab y-axis label, defaults to "Survival probability"
-#' @param .lwd plot line width, defaults to 1
+#' @param basekm \code{survfit} object
+#' @param at vector of times on which to condition
+#' @param main plot title
+#' @param xlab x-axis label
+#' @param ylab y-axis label, defaults to "Survival probability"
+#' @param lwd plot line width, defaults to 1
 #'
 #' @return A ggplot with a line for the overall Kaplan-Meier plot and one
-#' additional line for each value in \code{.at}
+#' additional line for each value in \code{at}
 #'
 #' @export
 #'
 
+condKMggplot <- function(basekm,
+                         at,
+                         main = NULL,
+                         xlab = "Years",
+                         ylab = "Survival probability",
+                         lwd = 1) {
 
-condKMggplot <- function(.basekm, .at, .main = NULL, .xlab = "Years",
-                         .ylab = "Survival probability", .lwd = 1) {
-
-  library(survival)
-  library(dplyr)
-  library(ggplot2)
-  library(purrr)
-
-  if(class(.basekm) != "survfit"){
-    stop("Argument to .basekm must be of class survfit")}
-  if(max(.at) > max(.basekm$time)){stop(paste(
-    "Argument to .at specifies value(s) outside the range of observed times;",
-    "the maximum observed time is", round(max(.basekm$time),2)))}
-
-  .at <- c(0, .at)
-  nt <- length(.at)
-  fitkm <- list()
-  fitkmdat <- list()
-  for(i in 1:nt){
-    fitkm[[i]] <- survfit(formula = as.formula(.basekm$call$formula),
-                          data = eval(.basekm$call$data),
-                          start.time = .at[i])
-    fitkmdat[[i]] <- data_frame(time = fitkm[[i]]$time,
-                                prob = fitkm[[i]]$surv)
+  if(class(basekm) != "survfit") {
+    stop(
+      "Argument to basekm must be of class survfit"
+    )
   }
 
-  condsurvdat <- fitkmdat %>%
-    map_df(`[`, .id = "condtime") %>%
-    mutate(condtime = factor(condtime, labels = .at))
+  if(max(at) > max(basekm$time)) {
+    stop(
+      paste(
+        "Argument to at specifies value(s) outside the range of observed times;",
+    "the maximum observed time is", round(max(.basekm$time),2)
+      )
+    )
+    }
 
-  ggplot(condsurvdat, aes(x = time, y = prob, color = condtime)) +
-    geom_step(lwd = .lwd) +
-    ylim(0, 1) +
-    xlab(.xlab) +
-    ylab(.ylab) +
-    labs(color = "x") +
-    ggtitle(.main) +
-    theme_bw()
+  at <- c(0, at)
+
+  nt <- length(at)
+
+  fitkm <- list()
+  fitkmdat <- list()
+
+  for(i in 1:nt) {
+    fitkm[[i]] <- survival::survfit(
+      formula = as.formula(basekm$call$formula),
+      data = eval(basekm$call$data),
+      start.time = at[i])
+
+    fitkmdat[[i]] <- tibble::tibble(
+      time = fitkm[[i]]$time,
+      prob = fitkm[[i]]$surv)
+    }
+
+  condsurvdat <- fitkmdat %>%
+    purrr::map_df(`[`, .id = "condtime") %>%
+    dplyr::mutate(condtime = factor(condtime, labels = at))
+
+  ggplot2::ggplot(condsurvdat,
+                  ggplot2::aes(x = time, y = prob, color = condtime)) +
+    ggplot2::geom_step(lwd = lwd) +
+    ggplot2::ylim(0, 1) +
+    ggplot2::labs(
+      x = xlab,
+      y = ylab,
+      title = main
+    ) +
+    ggplot2::labs(color = "x") +
+    ggplot2::theme_bw()
 }
--- code_to_create_condsurv_pdf.R
+++
@@ -1,3 +0,0 @@
-pack <- "condsurv"
-path <- find.package(pack)
-system(paste(shQuote(file.path(R.home("bin"), "R")),"CMD", "Rd2pdf", shQuote(path)))
